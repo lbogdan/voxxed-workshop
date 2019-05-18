425dc525425875ebe125f7142569fc249893fb8f
Bogdan Luca <luca.bogdan@gmail.com>
2019-05-19 01:37:12 +0300

    MovieCard: extract content to internal state.

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="card horizontal row">
     <div class="card-image col s3">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51poKKV63GL.jpg"
-      />
+      <img :src="movie.poster" />
     </div>
     <div class="card-stacked col s9">
       <div class="card-content">
         <div class="card-header">
           <div class="card-title">
-            Star Wars: The Last Jedi
-            <span class="year">(2017)</span>
+            {{ movie.title }}
+            <span class="year">({{ movie.year }})</span>
           </div>
-          <span class="genre">Action, Adventure, Fantasy</span>
+          <span class="genre">{{ movie.genre }}</span>
         </div>
-        <p>
-          Rey develops her newly discovered abilities with the guidance of Luke
-          Skywalker, who is unsettled by the strength of her powers. Meanwhile,
-          the Resistance prepares for battle with the First Order.
-        </p>
+        <p>{{ movie.plot }}</p>
         <div class="card-action">
           <a class="btn indigo accent-2" href="#" disabled>Edit movie</a>&#8203;
           <button class="btn deep-orange" disabled>Delete movie</button>
@@ -27,6 +21,25 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'movie-card',
+  data() {
+    return {
+      movie: {
+        title: 'Star Wars: The Last Jedi',
+        year: 2017,
+        genre: 'Action, Adventure, Fantasy ',
+        plot:
+          'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares for battle with the First Order.',
+        poster:
+          'https://images-na.ssl-images-amazon.com/images/I/51poKKV63GL.jpg',
+      },
+    };
+  },
+};
+</script>
 
 <style>
 .card {
